647aa932627fd3bb4b0855a5fa2b571bcc548847
GorobetsAndrii <43909562+GorobetsAndrii@users.noreply.github.com>
2019-04-22 13:23:13 +0200

    Add files via upload

--- Neural Network v1.0/src/sample/Controller.java
+++ Neural Network v1.0/src/sample/Controller.java
@@ -1,4 +1,114 @@
 package sample;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.scene.layout.VBox;
+import javafx.scene.shape.Rectangle;
+
 public class Controller {
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private VBox vbox;
+
+    @FXML
+    private AnchorPane mainpane;
+
+    @FXML
+    private Pane window;
+
+    @FXML
+    private Rectangle pixel1;
+
+    @FXML
+    private Rectangle pixel2;
+
+    @FXML
+    private Rectangle pixel3;
+
+    @FXML
+    private Rectangle pixel4;
+
+    @FXML
+    private Rectangle pixel5;
+
+    @FXML
+    private Rectangle pixel6;
+
+    @FXML
+    private Rectangle pixel7;
+
+    @FXML
+    private Rectangle pixel8;
+
+    @FXML
+    private Rectangle pixel9;
+
+    @FXML
+    private Rectangle pixel10;
+
+    @FXML
+    private Rectangle pixel11;
+
+    @FXML
+    private Rectangle pixel12;
+
+    @FXML
+    private Rectangle pixel15;
+
+    @FXML
+    private Rectangle pixel14;
+
+    @FXML
+    private Rectangle pixel13;
+
+    @FXML
+    private Button reset;
+
+    @FXML
+    private Button predict;
+
+    @FXML
+    private AnchorPane panetitle;
+
+    @FXML
+    private Label title;
+
+    @FXML
+    void initialize() {
+        assert vbox != null : "fx:id=\"vbox\" was not injected: check your FXML file 'Scene1full.fxml'.";
+        assert mainpane != null : "fx:id=\"mainpane\" was not injected: check your FXML file 'Scene1full.fxml'.";
+        assert window != null : "fx:id=\"window\" was not injected: check your FXML file 'Scene1full.fxml'.";
+        assert pixel1 != null : "fx:id=\"pixel1\" was not injected: check your FXML file 'Scene1full.fxml'.";
+        assert pixel2 != null : "fx:id=\"pixel2\" was not injected: check your FXML file 'Scene1full.fxml'.";
+        assert pixel3 != null : "fx:id=\"pixel3\" was not injected: check your FXML file 'Scene1full.fxml'.";
+        assert pixel4 != null : "fx:id=\"pixel4\" was not injected: check your FXML file 'Scene1full.fxml'.";
+        assert pixel5 != null : "fx:id=\"pixel5\" was not injected: check your FXML file 'Scene1full.fxml'.";
+        assert pixel6 != null : "fx:id=\"pixel6\" was not injected: check your FXML file 'Scene1full.fxml'.";
+        assert pixel7 != null : "fx:id=\"pixel7\" was not injected: check your FXML file 'Scene1full.fxml'.";
+        assert pixel8 != null : "fx:id=\"pixel8\" was not injected: check your FXML file 'Scene1full.fxml'.";
+        assert pixel9 != null : "fx:id=\"pixel9\" was not injected: check your FXML file 'Scene1full.fxml'.";
+        assert pixel10 != null : "fx:id=\"pixel10\" was not injected: check your FXML file 'Scene1full.fxml'.";
+        assert pixel11 != null : "fx:id=\"pixel11\" was not injected: check your FXML file 'Scene1full.fxml'.";
+        assert pixel12 != null : "fx:id=\"pixel12\" was not injected: check your FXML file 'Scene1full.fxml'.";
+        assert pixel15 != null : "fx:id=\"pixel15\" was not injected: check your FXML file 'Scene1full.fxml'.";
+        assert pixel14 != null : "fx:id=\"pixel14\" was not injected: check your FXML file 'Scene1full.fxml'.";
+        assert pixel13 != null : "fx:id=\"pixel13\" was not injected: check your FXML file 'Scene1full.fxml'.";
+        assert reset != null : "fx:id=\"reset\" was not injected: check your FXML file 'Scene1full.fxml'.";
+        assert predict != null : "fx:id=\"predict\" was not injected: check your FXML file 'Scene1full.fxml'.";
+        assert panetitle != null : "fx:id=\"panetitle\" was not injected: check your FXML file 'Scene1full.fxml'.";
+        assert title != null : "fx:id=\"title\" was not injected: check your FXML file 'Scene1full.fxml'.";
+
+    }
 }
+
